bbb8334719ef53bffcbb2aeedc02b4cbdcba55a4
ALEXEY <velichkoalsprog@gmail.com>
2022-06-30 18:38:31 +0300

    feat: add header and converter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.component';
+import { ConverterComponent } from './converter/converter.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConverterComponent
   ],
   imports: [
     BrowserModule
